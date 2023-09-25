eb8606dc635f1263f602432ad59667fdecab012e
user <prithvirajpowar20@gmail.com>
2023-09-25 09:31:57 +0530

    this is commit for web app

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import pandas as pd
 
 app=Flask(__name__)
 ## Load the model
-regmodel=pickle.load(open('regmodel.pkl','rb'))
-scalar=pickle.load(open(r'C:\Users\prithviraj\OneDrive\Desktop\Datascience\ETL using apache spark\scaling.pkl','rb'))
+regmodel=pickle.load(open(r'regmodel.pkl','rb'))
+scalar=pickle.load(open(r'scaling.pkl','rb'))
 @app.route('/')
 def home():
     return render_template('home.html')
